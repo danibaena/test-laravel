94377c3a41ff56918d7e2268e1bb6cd40b36a247
Daniel Baena <d.baena.hermosa@gmail.com>
2016-01-28 11:11:43 +0100

    Added favicon from the noun project site, and wip the UI

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <script src="assets/js/main.js"></script>
@@ -13,20 +14,25 @@
 
 <div class="container">
   <h2>Test Laravel</h2>
-  <p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>            
+  <p>Show a list of users, movies they're subscribed to and the status of the subscription</p>            
   <table class="table">
     <thead>
       <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
+        <th>User</th>
+        <th>Movies</th>
+        <th>Subscription Status</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
+        <td>Marie</td>
+        <td>Uno de los nuestros</td>
+        <td>Seen</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Pulp Fiction</td>
+        <td>Pending</td>
       </tr>
       <tr>
         <td>Mary</td>
