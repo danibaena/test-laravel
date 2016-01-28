c844d0615e43ab815d8949cfc860c25ab352c1f1
Daniel Baena <d.baena.hermosa@gmail.com>
2016-01-28 10:18:12 +0100

    Finished UsersController. API is now ready to be consumed.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,18 +20,16 @@ class UsersController extends Controller
     public function index()
     {
         $users = DB::table('users')->select('id', 'username')->distinct()->get();
-        $movies = DB::table('movies')->select()->distinct()->get();
 
         foreach($users as $user){
             $user_movies = DB::table('user_movie')->select('movie_id', 'status')
                 ->where('user_id', '=', $user->id)
                 ->get();
-            $user->movies = [];
             foreach($user_movies as $user_movie){
                 $movie = DB::table('movies')->select()->distinct()
                     ->where('id', '=', $user_movie->movie_id)->get();
                 $movie[0]->status = $user_movie->status;
-                $user->movies = $movie;
+                $user->movies[] = $movie[0];
             }
         }
         $responseArray = [
